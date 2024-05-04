714a67a7abf92a9948f7c983646af1e15acf064d
saeedbaf <157725921+saeedbaf@users.noreply.github.com>
2024-05-04 16:02:36 -0400

    Update and rename mainGUI to mainGUI.java

--- mainGUI.java
+++ mainGUI.java
@@ -17,19 +17,28 @@ public class mainGUI extends JFrame implements Serializable{
 		//Runtime.getRuntime().addShutdownHook(new Thread(() -> saveData()));
 		Administrator superadmin=new Administrator("superadmin","0","123");
 		admins.add(superadmin);
-		Student stud1=new Student("1","1","HAPPYA","Freshman");
+		Student stud1=new Student("1","1","HAPPYA","Freshaman");
 		Student stud2=new Student("2","11","HAPPYFAKE","Senior");
 		students.add(stud1);
 		students.add(stud2);
-		StudentCourse orgo=new StudentCourse("orgo");
-		StudentCourse cs=new StudentCourse("basic");
-		StudentCourse orgo2=new StudentCourse("orgo2");
+		StudentCourse orgo=new StudentCourse("orgo","CHEM-UA-125",5);
+		StudentCourse cs=new StudentCourse("basic","CSCI-310",4);
+		StudentCourse orgo2=new StudentCourse("orgo2","CHEM-UA-126",5);
+		Courses newchem=new Courses("orgo", "CHEM-UA-125", 5,  "Bobst","Mon 5:00PM-6:15PM", null);
+		courses.add(newchem);
+		Courses newchem1=new Courses("orgo2", "CHEM-UA-126", 5,  "Bobst","Mon 8:00AM-9:15AM", courses);
+		courses.add(newchem1);
+		Courses newcs=new Courses("basic", "CSCI-310", 4,  "Silver","Tue 5:00PM-6:15PM", null);
+		courses.add(newcs);
+		orgo.setGrade("A");
+		cs.setGrade("B+");
+		orgo2.setGrade("C-");
 		ArrayList<StudentCourse> stucourses=new ArrayList<StudentCourse>();
 		stucourses.add(cs);
-		stucourses.add(orgo);
 		stud1.setCourses(stucourses);
-		stucourses.add(orgo2);
+		stucourses.add(orgo);
 		stud2.setCourses(stucourses);
+		stucourses.add(orgo2);
 		setTitle("Login");
         setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,7 +62,7 @@ public class mainGUI extends JFrame implements Serializable{
             	if(admin.getID().equals(id)) {
             		if(admin.getPassword().equals(password)) {
             			isadmin=true;
-            			//this.dispose();
+            			this.dispose();
             			adminGUI ad=new adminGUI(id, admins, students, courses);
             			for(Courses newcourse:ad.newcourses) {
             				courses.add(newcourse);
@@ -70,6 +79,8 @@ public class mainGUI extends JFrame implements Serializable{
                 	if(stud.getID().equals(id)) {
                 		if(stud.getPassword().equals(password)) {
                 			isstudent=true;
+                			this.dispose();
+                			new studentGUI(stud,courses);
                 		}
                 	
             }
