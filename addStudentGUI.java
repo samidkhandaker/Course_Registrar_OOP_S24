2f64bd41d05ac27ef7d35e359bddedd8c89caea6
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-04 18:30:23 -0400

    Update addStudentGUI.java

--- addStudentGUI.java
+++ addStudentGUI.java
@@ -12,25 +12,33 @@ public class addStudentGUI extends JFrame {
 
     public addStudentGUI() {
         setTitle("Add Student");
-        setSize(400, 200);
+        setSize(400, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 2));
 
         JLabel idLabel = new JLabel("ID:");
+        idLabel.setBounds(95, 31, 18, 16);
         JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(50, 69, 63, 16);
         JLabel nameLabel = new JLabel("Name:");
+        nameLabel.setBounds(73, 104, 40, 16);
         JLabel yearLabel = new JLabel("Year:");
+        yearLabel.setBounds(82, 138, 31, 16);
 
         idField = new JTextField(15);
+        idField.setBounds(128, 26, 190, 26);
         passwordField = new JTextField(15);
+        passwordField.setBounds(128, 64, 190, 26);
         nameField = new JTextField(15);
+        nameField.setBounds(128, 99, 190, 26);
         String[] years = {"Freshman", "Sophomore", "Junior", "Senior"};
         yearComboBox = new JComboBox<>(years);
+        yearComboBox.setBounds(128, 134, 127, 27);
 
         JButton addButton = new JButton("Add");
+        addButton.setBounds(211, 176, 86, 29);
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -44,12 +52,14 @@ public class addStudentGUI extends JFrame {
             }
         });
         JButton cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(113, 176, 86, 29);
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose(); // Close the dialog
             }
         });
+        panel.setLayout(null);
 
         panel.add(idLabel);
         panel.add(idField);
@@ -62,7 +72,7 @@ public class addStudentGUI extends JFrame {
         panel.add(cancelButton); // empty label for spacing
         panel.add(addButton);
 
-        add(panel);
+        getContentPane().add(panel);
         setVisible(true);
     }
 
