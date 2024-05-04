cf54e78a2711e651ed3d4cf929314847f65f1039
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-04 18:12:08 -0400

    Update editDialog.java

--- editDialog.java
+++ editDialog.java
@@ -11,20 +11,26 @@ public class editDialog extends JDialog{
 
     public editDialog(JFrame parent) {
     	super(parent, "Edit Profile", true); // true for modal dialog
-        setSize(700, 300);
+        setSize(467, 300);
         setLocationRelativeTo(parent);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 2));
+        panel.setBounds(0, 0, 467, 272);
 
         JLabel nameLabel = new JLabel("New Name:");
+        nameLabel.setBounds(74, 93, 71, 16);
         JLabel passwordLabel = new JLabel("New Password:");
+        passwordLabel.setBounds(51, 163, 94, 16);
         JLabel infoLabel = new JLabel("Leave the field empty to keep the current name or password");
-
+        infoLabel.setBounds(39, 36, 377, 16);
+	    
         nameField = new JTextField(15);
+        nameField.setBounds(157, 88, 259, 26);
         passwordField = new JTextField(15);
+        passwordField.setBounds(157, 158, 259, 26);
 
         JButton saveButton = new JButton("Save");
+        saveButton.setBounds(242, 222, 86, 29);
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -34,24 +40,25 @@ public class editDialog extends JDialog{
             }
         });
         JButton cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(129, 222, 86, 29);
         cancelButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose(); // Close the dialog
             }
         });
+        getContentPane().setLayout(null);
+        panel.setLayout(null);
 
         panel.add(nameLabel);
         panel.add(nameField);
         panel.add(passwordLabel);
         panel.add(passwordField);
         panel.add(infoLabel); // Info label for instructions
-        panel.add(new JLabel()); // Empty label for spacing
-       // panel.add(new JLabel()); // Empty label for spacing
         panel.add(cancelButton);
         panel.add(saveButton);
 
-        add(panel);
+        getContentPane().add(panel);
         setVisible(true);
     }
 
