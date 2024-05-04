d95d3ba89faeb0d95df85b11c00b74c884472223
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-04 17:58:53 -0400

    Update adminGUI.java

--- adminGUI.java
+++ adminGUI.java
@@ -9,20 +9,37 @@ public class adminGUI extends JFrame{
 	//public String id;
 	public ArrayList<Courses> newcourses=new ArrayList<Courses>();
 	public ArrayList<Student> newstudents=new ArrayList<Student>();
+	public int index = 0;
 	public adminGUI(String id, ArrayList<Administrator> admins, ArrayList<Student> students, ArrayList<Courses> course) {
         setTitle("Admin Panel");
-        setSize(300, 200);
+        setSize(375, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 1));
 
         JButton addClassButton = new JButton("Add Course");
+        addClassButton.setBounds(186, 66, 164, 39);
         JButton updateGradesButton = new JButton("Update Grades");
+        updateGradesButton.setBounds(186, 117, 164, 39);
         JButton addStudentButton = new JButton("Add Student");
+        addStudentButton.setBounds(186, 168, 164, 39);
         JButton editProfileButton = new JButton("Edit Profile");
+        editProfileButton.setBounds(26, 66, 112, 39);
 
+	int cur = 0;
+        for(Administrator admin:admins) {
+            if(admin.getID().equals(id)) {
+            	index=cur;
+            	break;
+            }
+            cur++;
+        }
+        
+	JLabel nameLabel = new JLabel("Admin Name: " + admins.get(index).getName());
+        nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        nameLabel.setBounds(16, 16, 334, 29);
+		
         addClassButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	addClassGUI ad=new addClassGUI(course);
@@ -51,6 +68,7 @@ public class adminGUI extends JFrame{
                     if(admin.getID().equals(id)) {
                         if(!newName.isEmpty()) {
                             admin.setName(newName);
+			    nameLabel.setText("Admin Name: " + admins.get(index).getName());
                         }
                         if(!newPassword.isEmpty()) {
                             admin.setPassword(newPassword);
@@ -61,14 +79,15 @@ public class adminGUI extends JFrame{
               //  dispose();
             }
         });
-
+        panel.setLayout(null);
 
         panel.add(addClassButton);
         panel.add(updateGradesButton);
         panel.add(addStudentButton);
         panel.add(editProfileButton);
+	panel.add(nameLabel);
 
-        add(panel);
+        getContentPane().add(panel);
         setVisible(true);
 }
 }
