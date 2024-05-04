67d262dc908f6bbaeb8d2bbac815190f0452d8a5
saeedbaf <157725921+saeedbaf@users.noreply.github.com>
2024-05-04 16:03:09 -0400

    Update StudentCourse.java

--- StudentCourse.java
+++ StudentCourse.java
@@ -2,12 +2,22 @@ package OOPPROJECT;
 
 public class StudentCourse{
 	public String name;
+	public String symbol;
 	public String grade;
-	public StudentCourse(String name) {
+	public int credits;
+	public StudentCourse(String name, String symbol ,int credits) {
 		this.name=name;
+		this.symbol=symbol;
+		this.credits=credits;
 	  }
 	public String getName() {
 		return this.name;
+	}
+	public void setSymbol(String symbol) {
+		 this.symbol=symbol;
+	}
+	public String getSymbol() {
+		return this.symbol;
 	}
 	public void setName(String name) {
 		 this.name=name;
@@ -17,6 +27,12 @@ public class StudentCourse{
 	}
 	public void setGrade(String grade) {
 		 this.grade=grade;
+	}
+	public int getCredits() {
+		return this.credits;
+	}
+	public void setCredits(int credits) {
+		 this.credits=credits;
 	}
 	
 }
