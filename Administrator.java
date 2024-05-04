c5a8985ce310e2371846f526ee925a41005ddbd6
saeedbaf <157725921+saeedbaf@users.noreply.github.com>
2024-05-03 23:32:14 -0400

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -6,12 +6,10 @@ public class Administrator extends UniversityPersonnel{
 		super(id, password);
 		this.name=name;
 	}
-	public void setGrade(Student stu, String courseName, String grade ) {
-		for(StudentCourse course: stu.courses) {
-			if(course.getName().equals(courseName)) {
-				course.grade=grade;
-				return;
-			}
-		}
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name=name;
 	}
 }
