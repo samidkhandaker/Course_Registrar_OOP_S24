aa1c3967c33b005c2c10e3510257b7b49f92a758
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-05 19:59:28 -0400

    Update Courses.java

--- Courses.java
+++ Courses.java
@@ -1,5 +1,3 @@
-package OOPPROJECT;
-
 import java.util.ArrayList;
 
 public class Courses {
@@ -10,97 +8,112 @@ public class Courses {
 	public int enrollment;
 	public String location;
 	public String time;
-	public ArrayList<Courses> prerequisites=new ArrayList<Courses>();
-	public Courses(String name, String symbol, int credit,  String location,String time, ArrayList<Courses> prerequisites) {
-		  this.name=name;
-		  this.symbol=symbol;
-		  this.credit=credit;
-		  this.location=location;
-		  setCapacity(location);
-		  this.enrollment=0;
-		  this.time=time;
-		  if(prerequisites!=null) {
-		  this.prerequisites=new ArrayList<Courses>(prerequisites);
-		  }
-	  }
+	public ArrayList<Courses> prerequisites = new ArrayList<Courses>();
+
+	public Courses(String name, String symbol, int credit, String location, String time,
+			ArrayList<Courses> prerequisites) {
+		this.name = name;
+		this.symbol = symbol;
+		this.credit = credit;
+		this.location = location;
+		setCapacity(location);
+		this.enrollment = 0;
+		this.time = time;
+		if (prerequisites != null) {
+			this.prerequisites = new ArrayList<Courses>(prerequisites);
+		}
+	}
+
 	public void setName(String name) {
-		  this.name=name;
-	  }
+		this.name = name;
+	}
+
 	public void setSymbol(String symbol) {
-		  this.symbol=symbol;
-	  }
+		this.symbol = symbol;
+	}
+
 	public void setCredit(int credit) {
-		  this.credit=credit;
-	  }
+		this.credit = credit;
+	}
+
 	public void setlocation(String location) {
-		  this.location=location;
-	  }
+		this.location = location;
+	}
+
 	public void setTime(String time) {
-		  this.time=time;
-	  }
+		this.time = time;
+	}
+
 	public void setprerequisites(ArrayList<Courses> prerequisites) {
-		  this.prerequisites=prerequisites;
-	  }
+		this.prerequisites = prerequisites;
+	}
+
 	public String getName() {
-		  return this.name;
-	  }
+		return this.name;
+	}
+
 	public String getSymbol() {
-		  return this.symbol;
-	  }
+		return this.symbol;
+	}
+
 	public int getCredit() {
-		  return this.credit;
-	  }
+		return this.credit;
+	}
+
 	public String getLocation() {
-		  return this.location;
-	  }
+		return this.location;
+	}
+
 	public String getTime() {
-		  return this.time;
-	  }
+		return this.time;
+	}
+
 	public ArrayList<Courses> getPrerequisites() {
-		  return this.prerequisites;
-	  }
+		return this.prerequisites;
+	}
+
 	public boolean enroll() {
-		if(enrollment<capacity) {
+		if (enrollment < capacity) {
 			enrollment++;
 			return true;
 		}
 		return false;
 	}
+
 	public void deenroll() {
 		enrollment--;
 	}
+
 	public boolean meetRequirments(Student stud) {
-		if(prerequisites==null || prerequisites.isEmpty()) {
+		if (prerequisites == null || prerequisites.isEmpty()) {
 			return true;
 		}
-		ArrayList<StudentCourse> transcript=stud.getCourses();
-		boolean check=false;
-		for(Courses course:prerequisites) {
-			for(StudentCourse studcourse:transcript) {
-				if(course.getName().equals(studcourse.getName())) {
-					check=true;
+		ArrayList<StudentCourse> transcript = stud.getCourses();
+		boolean check = false;
+		for (Courses course : prerequisites) {
+			for (StudentCourse studcourse : transcript) {
+				if (course.getName().equals(studcourse.getName())) {
+					check = true;
 					break;
 				}
 			}
-			if(!check) {
+			if (!check) {
 				return false;
 			}
-			check=false;
+			check = false;
 		}
 		return true;
 	}
+
 	private void setCapacity(String location) {
-		if(location.equals("Bobst")) {
-			this.capacity=20;
-		}
-		else if(location.equals("Silver")) {
-			this.capacity=60;
-		}
-		else if(location.equals("Cantor")) {
-			this.capacity=100;
-		}
-		else if(location.equals("Paulson")) {
-			this.capacity=25;
+		if (location.equals("Bobst")) {
+			this.capacity = 20;
+		} else if (location.equals("Silver")) {
+			this.capacity = 60;
+		} else if (location.equals("Cantor")) {
+			this.capacity = 100;
+		} else if (location.equals("Paulson")) {
+			this.capacity = 25;
 		}
 	}
 }
