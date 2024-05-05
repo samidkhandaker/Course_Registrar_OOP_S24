18732184ba21bbc4ac89a187fc75d45cf3450ec4
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-05 19:59:07 -0400

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,15 +1,16 @@
-package OOPPROJECT;
-
-public class Administrator extends UniversityPersonnel{
+public class Administrator extends UniversityPersonnel {
 	public String name;
-	public Administrator(String name, String id, String password){
+
+	public Administrator(String name, String id, String password) {
 		super(id, password);
-		this.name=name;
+		this.name = name;
 	}
+
 	public String getName() {
 		return this.name;
 	}
+
 	public void setName(String name) {
-		this.name=name;
+		this.name = name;
 	}
 }
