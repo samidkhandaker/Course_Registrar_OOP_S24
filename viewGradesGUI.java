b4472a061679b496bdcefbf53fa2adb13b654a96
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-05 20:04:26 -0400

    Update viewGradesGUI.java

--- viewGradesGUI.java
+++ viewGradesGUI.java
@@ -1,4 +1,3 @@
-package OOPPROJECT;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -6,39 +5,42 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class viewGradesGUI extends JFrame {
-    private ArrayList<StudentCourse> courseList;
-    private JTextArea textArea;
+	private ArrayList<StudentCourse> courseList;
+	private JTextArea textArea;
 
-    public viewGradesGUI(Student student) {
+	public viewGradesGUI(Student student) {
 
-    	 setTitle("View Grades");
-         setSize(300, 300);
-         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-         setLocationRelativeTo(null);
+		setTitle("View Grades");
+		setSize(319, 311);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(0, 1));
+		JPanel panel = new JPanel();
 
-        JTextArea textArea = new JTextArea(10, 20);
-        textArea.setEditable(false);
-        textArea.setText(student.getTranscript());
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        String gpa=String.format("%.4f", student.getGPA());
-        JLabel gpaLabel = new JLabel("Final GPA: " +gpa );
-        JButton cancelButton = new JButton("Back");
+		JTextArea textArea = new JTextArea(10, 20);
+		textArea.setEditable(false);
+		textArea.setText(student.getTranscript());
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		scrollPane.setBounds(28, 23, 265, 175);
+		String gpa = String.format("%.4f", student.getGPA());
+		JLabel gpaLabel = new JLabel("Final GPA: " + gpa);
+		gpaLabel.setBounds(103, 210, 111, 16);
+		JButton cancelButton = new JButton("Back");
+		cancelButton.setBounds(125, 238, 75, 29);
+		panel.setLayout(null);
 
-        panel.add(scrollPane);
-        panel.add(gpaLabel);
-        panel.add(cancelButton);
+		panel.add(scrollPane);
+		panel.add(gpaLabel);
+		panel.add(cancelButton);
 
-        cancelButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
-        });
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 
-        add(panel);
-        setVisible(true);
-    }
+		getContentPane().add(panel);
+		setVisible(true);
+	}
 }
