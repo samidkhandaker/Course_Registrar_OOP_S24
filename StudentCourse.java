1a8be4da4bb8b2dc5436cc9c852720e125c96af7
ryzhao123 <98000484+ryzhao123@users.noreply.github.com>
2024-05-05 20:00:09 -0400

    Update StudentCourse.java

--- StudentCourse.java
+++ StudentCourse.java
@@ -1,38 +1,45 @@
-package OOPPROJECT;
-
-public class StudentCourse{
+public class StudentCourse {
 	public String name;
 	public String symbol;
 	public String grade;
 	public int credits;
-	public StudentCourse(String name, String symbol ,int credits) {
-		this.name=name;
-		this.symbol=symbol;
-		this.credits=credits;
-	  }
+
+	public StudentCourse(String name, String symbol, int credits) {
+		this.name = name;
+		this.symbol = symbol;
+		this.credits = credits;
+	}
+
 	public String getName() {
 		return this.name;
 	}
+
 	public void setSymbol(String symbol) {
-		 this.symbol=symbol;
+		this.symbol = symbol;
 	}
+
 	public String getSymbol() {
 		return this.symbol;
 	}
+
 	public void setName(String name) {
-		 this.name=name;
+		this.name = name;
 	}
+
 	public String getGrade() {
 		return this.grade;
 	}
+
 	public void setGrade(String grade) {
-		 this.grade=grade;
+		this.grade = grade;
 	}
+
 	public int getCredits() {
 		return this.credits;
 	}
+
 	public void setCredits(int credits) {
-		 this.credits=credits;
+		this.credits = credits;
 	}
-	
+
 }
